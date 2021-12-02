6dbef701b7117c7a26b51e523636e9171a84989b
cy77cc <483564813@qq.com>
2021-12-02 23:15:08 +0800

    add:876

---
+++ 876. 链表的中间结点.go
@@ -0,0 +1,19 @@
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func middleNode(head *ListNode) *ListNode {
+	slow, fast := head, head
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+	return slow
+}
+
+func main() {
+
+}
